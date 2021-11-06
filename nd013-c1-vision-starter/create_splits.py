46dcd3d9f3ccb8728351b7445719807f4b37c5d1
melike <melike@leodrive.ai>
2021-11-06 20:31:48 +0300

    update split.py

--- nd013-c1-vision-starter/create_splits.py
+++ nd013-c1-vision-starter/create_splits.py
@@ -2,7 +2,7 @@ import argparse
 import glob
 import os
 import random
-
+import shutil
 import numpy as np
 
 from utils import get_module_logger
@@ -16,6 +16,49 @@ def split(data_dir):
     args:
         - data_dir [str]: data directory, /mnt/data
     """
+    print("*********************************")
+    print(data_dir)
+    
+    val_ratio = 0.20
+    test_ratio = 0.20
+    
+    src = data_dir  # Folder to copy images from
+
+    allFileNames = os.listdir(src)
+    print(allFileNames)
+    np.random.shuffle(allFileNames)
+    train_FileNames, val_FileNames, test_FileNames = np.split(np.array(allFileNames),
+                                                                  [int(len(allFileNames) * (1 - (val_ratio + test_ratio))),
+                                                                   int(len(allFileNames) * (1 - val_ratio)),
+                                                                   ])
+
+    train_FileNames = [src + '//' + name for name in train_FileNames.tolist()]
+    val_FileNames = [src + '//' + name for name in val_FileNames.tolist()]
+    test_FileNames = [src + '//' + name for name in test_FileNames.tolist()]
+    print(train_FileNames)
+    print("************")
+
+    print('Total images: '+ str(len(allFileNames)))
+    print('Training: '+ str(len(train_FileNames)))
+    print('Validation: '+  str(len(val_FileNames)))
+    print('Testing: '+ str(len(test_FileNames)))
+
+    root_dir = '/app/project/data'
+    # # Creating Train / Val / Test folders (One time use)
+    os.makedirs(root_dir + '/train/')
+    os.makedirs(root_dir + '/val/' )
+    os.makedirs(root_dir + '/test/')
+
+    # Copy-pasting images
+    for name in train_FileNames:
+        shutil.move(name, root_dir + '/train/')
+
+    for name in val_FileNames:
+        shutil.move(name, root_dir + '/val/')
+
+    for name in test_FileNames:
+        shutil.move(name, root_dir + '/test/')
+
     # TODO: Implement function
     
 
