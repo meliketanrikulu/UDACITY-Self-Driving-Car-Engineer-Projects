0a845fe0424ef35209406cb411e55a401ef5bd50
melike <41450930+meliketanrikulu@users.noreply.github.com>
2021-12-24 18:12:09 +0300

    added project2

---
+++ nd013-c2-fusion-starter/student/association.py
@@ -0,0 +1,152 @@
+# ---------------------------------------------------------------------
+# Project "Track 3D-Objects Over Time"
+# Copyright (C) 2020, Dr. Antje Muntzinger / Dr. Andreas Haja.
+#
+# Purpose of this file : Data association class with single nearest neighbor association and gating based on Mahalanobis distance
+#
+# You should have received a copy of the Udacity license together with this program.
+#
+# https://www.udacity.com/course/self-driving-car-engineer-nanodegree--nd013
+# ----------------------------------------------------------------------
+#
+
+# add project directory to python path to enable relative imports
+import os
+import sys
+
+# imports
+import numpy as np
+from scipy.stats.distributions import chi2
+
+PACKAGE_PARENT = '..'
+SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
+sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
+
+import misc.params as params
+
+
+class Association:
+    '''Data association class with single nearest neighbor association and gating based on Mahalanobis distance'''
+
+    def __init__(self):
+        self.association_matrix = np.matrix([])
+        self.unassigned_tracks = []
+        self.unassigned_meas = []
+
+    def associate(self, track_list, meas_list, KF):
+
+        ############
+        # TODO Step 3: association:
+        # - replace association_matrix with the actual association matrix based on Mahalanobis distance (see below) for all tracks and all measurements
+        # - update list of unassigned measurements and unassigned tracks
+        ############
+
+        for track in track_list:
+            res = []
+            for meas in meas_list:
+                MHD = self.MHD(track, meas, KF)
+                sensor = meas.sensor
+                if self.gating_ok(MHD, sensor):
+                    res.append(MHD)
+
+                else:
+                    res.append(np.inf)
+            association_matrix.append(res)
+        #Here we set all tracks and measurements to be unassigned, later on 
+        #we will finally assign them when calling get_closest_track_and_meas
+        self.unassigned_tracks = np.arange(len(track_list)).tolist()
+        self.unassigned_meas = np.arange(len(meas_list)).tolist()
+        
+        self.association_matrix = np.matrix(association_matrix)
+        ############
+        # END student code
+        ############ 
+
+    def get_closest_track_and_meas(self):
+        ############
+        # TODO Step 3: find closest track and measurement:
+        # - find minimum entry in association matrix
+        # - delete row and column
+        # - remove corresponding track and measurement from unassigned_tracks and unassigned_meas
+        # - return this track and measurement
+        ############
+        A = self.association_matrix
+        if np.min(A) == np.inf:
+            return np.nan, np.nan
+
+        # the following only works for at most one track and one measurement
+        i, j = np.unravel_index(np.argmin(self.association_matrix, axis=None), self.association_matrix.shape)
+        update_track = self.unassigned_tracks[i]
+        update_meas = self.unassigned_meas[j]
+
+        # remove from list
+        self.unassigned_tracks.remove(update_track)
+        self.unassigned_meas.remove(update_meas)
+        self.association_matrix = np.delete(self.association_matrix, i, 0)
+        self.association_matrix = np.delete(self.association_matrix, j, 1)
+
+        ############
+        # END student code
+        ############ 
+        return update_track, update_meas
+
+    def gating(self, MHD, sensor):
+        ############
+        # TODO Step 3: return True if measurement lies inside gate, otherwise False
+        ############
+
+        return MHD < chi2.ppf(params.gating_threshold, df=sensor.dim_meas)
+
+        ############
+        # END student code
+        ############ 
+
+    def MHD(self, track, meas, KF):
+        ############
+        # TODO Step 3: calculate and return Mahalanobis distance
+        ############
+
+        H = meas.sensor.get_H(track.x)
+        gamma = meas.z - meas.sensor.get_hx(track.x)
+        S = H @ track.P @ H.T + meas.R
+        mhd = gamma.T @ np.linalg.inv(S) @ gamma
+        #mahalanobis matrix = mhd
+        return mhd
+
+        ############
+        # END student code
+        ############
+
+    def associate_and_update(self, manager, meas_list, KF):
+        # associate measurements and tracks
+        self.associate(manager.track_list, meas_list, KF)
+
+        # update associated tracks with measurements
+        while self.association_matrix.shape[0] > 0 and self.association_matrix.shape[1] > 0:
+
+            # search for next association between a track and a measurement
+            ind_track, ind_meas = self.get_closest_track_and_meas()
+            if np.isnan(ind_track):
+                print('---no more associations---')
+                break
+            track = manager.track_list[ind_track]
+
+            # check visibility, only update tracks in fov    
+            if not meas_list[0].sensor.in_fov(track.x):
+                continue
+
+            # Kalman update
+            print('update track', track.id, 'with', meas_list[ind_meas].sensor.name, 'measurement', ind_meas)
+            KF.update(track, meas_list[ind_meas])
+
+            # update score and track state 
+            manager.handle_updated_track(track)
+
+            # save updated track
+            manager.track_list[ind_track] = track
+
+        # run track management 
+        manager.manage_tracks(self.unassigned_tracks, self.unassigned_meas, meas_list)
+
+        for track in manager.track_list:
+            print('track', track.id, 'score =', track.score)
---
+++ nd013-c2-fusion-starter/student/measurements.py
@@ -0,0 +1,223 @@
+# ---------------------------------------------------------------------
+# Project "Track 3D-Objects Over Time"
+# Copyright (C) 2020, Dr. Antje Muntzinger / Dr. Andreas Haja.
+#
+# Purpose of this file : Classes for sensor and measurement 
+#
+# You should have received a copy of the Udacity license together with this program.
+#
+# https://www.udacity.com/course/self-driving-car-engineer-nanodegree--nd013
+# ----------------------------------------------------------------------
+#
+
+# imports
+import numpy as np
+
+# add project directory to python path to enable relative imports
+import os
+import sys
+PACKAGE_PARENT = '..'
+SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
+sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
+import misc.params as params 
+
+class Sensor:
+    '''Sensor class including measurement matrix'''
+    def __init__(self, name, calib):
+        self.name = name
+        if name == 'lidar':
+            self.dim_meas = 3
+            # transformation sensor to vehicle coordinates equals identity matrix because lidar detections are already in vehicle coordinates
+            self.sens_to_veh = np.matrix(np.identity((4))) 
+            # angle of field of view in radians
+            self.fov = [-np.pi/2, np.pi/2] 
+        
+        elif name == 'camera':
+            self.dim_meas = 2
+            # transformation sensor to vehicle coordinates
+            self.sens_to_veh = np.matrix(calib.extrinsic.transform).reshape(4,4) 
+            # focal length i-coordinate
+            self.f_i = calib.intrinsic[0] 
+            # focal length j-coordinate
+            self.f_j = calib.intrinsic[1] 
+            # principal point i-coordinate
+            self.c_i = calib.intrinsic[2] 
+            # principal point j-coordinate
+            self.c_j = calib.intrinsic[3] 
+            # angle of field of view in radians, inaccurate boundary region was removed
+            self.fov = [-0.35, 0.35] 
+        # transformation vehicle to sensor coordinates    
+        self.veh_to_sens = np.linalg.inv(self.sens_to_veh) 
+    
+    def in_fov(self, x):
+        # check if an object x can be seen by this sensor
+        ############
+        # TODO Step 4: implement a function that returns True if x lies in the sensor's field of view, 
+        # otherwise False.
+        ############
+        
+        
+        ## check if an object x can be seen by this sensor
+        # transfer position measurement to homogeneous coordinates.  [x, y, z] to [x, y, z, 1]
+        # transfer homogeneous coordinates from vehicle coordinate to sensor coordinate
+        # set visibility to False
+        # check if the object is in the sensor FOV
+        
+        # create homogeneous coordinates
+        pos_veh = np.ones((4, 1)) 
+        pos_veh[0:3] = x[0:3] 
+        
+        # transform from vehicle to sensor coordinates
+        pos_sens = self.veh_to_sens*pos_veh 
+        visible = False
+        
+        # make sure to not divide by zero - we can exclude the whole negative x-range here
+        if pos_sens[0] > 0: 
+            alpha = np.arctan(pos_sens[1]/pos_sens[0]) # calc angle between object and x-axis
+            # no normalization needed because returned alpha always lies between [-pi/2, pi/2]
+            if alpha > self.fov[0] and alpha < self.fov[1]:
+                visible = True
+
+        return visible
+        
+        ############
+        # END student code
+        ############ 
+             
+    def get_hx(self, x):    
+        # calculate nonlinear measurement expectation value h(x)   
+        if self.name == 'lidar':
+            # homogeneous coordinates
+            pos_veh = np.ones((4, 1)) 
+            pos_veh[0:3] = x[0:3] 
+            # transform from vehicle to lidar coordinates
+            pos_sens = self.veh_to_sens*pos_veh 
+            return pos_sens[0:3]
+        elif self.name == 'camera':
+            
+            ############
+            # TODO Step 4: implement nonlinear camera measurement function h:
+            # - transform position estimate from vehicle to camera coordinates
+            # - project from camera to image coordinates
+            # - make sure to not divide by zero, raise an error if needed
+            # - return h(x)
+            ############
+
+            # homogeneous coordinates
+            pos_veh = np.ones((4, 1))  
+            pos_veh[0:3] = x[0:3]
+            # transform from vehicle to camera coordinates
+            pos_sens = self.veh_to_sens @ pos_veh  
+            i = self.c_i - self.f_i * pos_sens[1, 0] / pos_sens[0, 0]
+            j = self.c_j - self.f_j * pos_sens[2, 0] / pos_sens[0, 0]
+
+            return np.array([[i], [j]])
+        
+            ############
+            # END student code
+            ############ 
+        
+    def get_H(self, x):
+        # calculate Jacobian H at current x from h(x)
+        H = np.matrix(np.zeros((self.dim_meas, params.dim_state)))
+        R = self.veh_to_sens[0:3, 0:3] # rotation
+        T = self.veh_to_sens[0:3, 3] # translation
+        if self.name == 'lidar':
+            H[0:3, 0:3] = R
+        elif self.name == 'camera':
+            # check and print error message if dividing by zero
+            if R[0,0]*x[0] + R[0,1]*x[1] + R[0,2]*x[2] + T[0] == 0: 
+                raise NameError('Jacobian not defined for this x!')
+            else:
+                H[0,0] = self.f_i * (-R[1,0] / (R[0,0]*x[0] + R[0,1]*x[1] + R[0,2]*x[2] + T[0])
+                                    + R[0,0] * (R[1,0]*x[0] + R[1,1]*x[1] + R[1,2]*x[2] + T[1]) \
+                                        / ((R[0,0]*x[0] + R[0,1]*x[1] + R[0,2]*x[2] + T[0])**2))
+                H[1,0] = self.f_j * (-R[2,0] / (R[0,0]*x[0] + R[0,1]*x[1] + R[0,2]*x[2] + T[0])
+                                    + R[0,0] * (R[2,0]*x[0] + R[2,1]*x[1] + R[2,2]*x[2] + T[2]) \
+                                        / ((R[0,0]*x[0] + R[0,1]*x[1] + R[0,2]*x[2] + T[0])**2))
+                H[0,1] = self.f_i * (-R[1,1] / (R[0,0]*x[0] + R[0,1]*x[1] + R[0,2]*x[2] + T[0])
+                                    + R[0,1] * (R[1,0]*x[0] + R[1,1]*x[1] + R[1,2]*x[2] + T[1]) \
+                                        / ((R[0,0]*x[0] + R[0,1]*x[1] + R[0,2]*x[2] + T[0])**2))
+                H[1,1] = self.f_j * (-R[2,1] / (R[0,0]*x[0] + R[0,1]*x[1] + R[0,2]*x[2] + T[0])
+                                    + R[0,1] * (R[2,0]*x[0] + R[2,1]*x[1] + R[2,2]*x[2] + T[2]) \
+                                        / ((R[0,0]*x[0] + R[0,1]*x[1] + R[0,2]*x[2] + T[0])**2))
+                H[0,2] = self.f_i * (-R[1,2] / (R[0,0]*x[0] + R[0,1]*x[1] + R[0,2]*x[2] + T[0])
+                                    + R[0,2] * (R[1,0]*x[0] + R[1,1]*x[1] + R[1,2]*x[2] + T[1]) \
+                                        / ((R[0,0]*x[0] + R[0,1]*x[1] + R[0,2]*x[2] + T[0])**2))
+                H[1,2] = self.f_j * (-R[2,2] / (R[0,0]*x[0] + R[0,1]*x[1] + R[0,2]*x[2] + T[0])
+                                    + R[0,2] * (R[2,0]*x[0] + R[2,1]*x[1] + R[2,2]*x[2] + T[2]) \
+                                        / ((R[0,0]*x[0] + R[0,1]*x[1] + R[0,2]*x[2] + T[0])**2))
+        return H   
+        
+    def generate_measurement(self, num_frame, z, meas_list):
+        
+        # generate new measurement from this sensor and add to measurement list
+        ############
+        # TODO Step 4: remove restriction to lidar in order to include camera as well
+        ############
+        
+        # code for only return lidar sensor measurement
+        meas = Measurement(num_frame, z, self)
+        meas_list.append(meas)
+        return meas_list
+        ############
+        # END student code
+        ############ 
+        
+        
+################### 
+        
+class Measurement:
+    '''Measurement class including measurement values, covariance, timestamp, sensor'''
+    def __init__(self, num_frame, z, sensor):
+        # create measurement object
+        self.t = (num_frame - 1) * params.dt # time
+        if sensor.name == 'lidar':
+            sigma_lidar_x = params.sigma_lidar_x # load params
+            sigma_lidar_y = params.sigma_lidar_y
+            sigma_lidar_z = params.sigma_lidar_z
+            # measurement vector
+            self.z = np.zeros((sensor.dim_meas,1)) 
+            self.z[0] = z[0]
+            self.z[1] = z[1]
+            self.z[2] = z[2]
+            # sensor that generated this measurement
+            self.sensor = sensor 
+            # measurement noise covariance matrix
+            self.R = np.matrix([[sigma_lidar_x**2, 0, 0], 
+                                [0, sigma_lidar_y**2, 0], 
+                                [0, 0, sigma_lidar_z**2]])
+            
+            self.width = z[4]
+            self.length = z[5]
+            self.height = z[3]
+            self.yaw = z[6]
+        elif sensor.name == 'camera':
+            
+            ############
+            # TODO Step 4: initialize camera measurement including z, R, and sensor 
+            ############
+            
+            # load sigma parameters
+            # load params
+            sigma_cam_i = params.sigma_cam_i  
+            sigma_cam_j  = params.sigma_cam_j 
+            
+            # create measurement vector
+            self.z = np.zeros((sensor.dim_meas,1))
+            self.z[0] = z[0]
+            self.z[1] = z[1]
+            
+            # create sensor object that generated this measurement
+            self.sensor = sensor
+            
+            # create noise covariance matrix
+            R = np.zeros((2,2))
+            R[0, 0] = sigma_cam_i**2
+            R[1, 1] = sigma_cam_j**2
+            self.R = np.matrix(R)
+            
+        
+            ############
+            # END student code
+            ############ 
